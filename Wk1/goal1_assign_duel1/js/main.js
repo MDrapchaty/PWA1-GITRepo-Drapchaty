07d8e2fb7e7f00fbb4e26243c44f31b97f71581e
MDrapchaty <drapchaty@Drapchatys-MacBook-Pro.local>
2015-09-05 17:12:28 -0700

    working

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -34,8 +34,15 @@
             var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
             var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
 
-            console.log(f1);
-            console.log(f2);
+            //console.log(f1);  testing
+            //console.log(f2);
+
+            //inflict damage
+            player1Health -= f1;
+            player2Health -= f2;
+
+            console.log(player1Name + ":" + player1Health + " " + player2Name + ":" + player2Health);
+
         };
 
     };
